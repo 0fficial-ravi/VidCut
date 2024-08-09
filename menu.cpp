83d7a0b78b1115352e005595997629eb17095b02
Ravi <ravisharmarock007@gmail.com>
2024-08-10 01:24:13 +0530

    ADD FUNCTIONAL AUDIO FORMAT FN
    added functional audio format function and get I/O file location for
    convertion

--- menu.cpp
+++ menu.cpp
@@ -4,9 +4,50 @@
 using namespace std;
 
 string currPage="main";
+string targetFileLoc="";
+string destLoc="~/Desktop/";
+string outFileName;
+string targetFileName;
 int choice;
 int mainMenuFlag;
 
+void getTargetFileName()
+{
+    for(int i=targetFileLoc.length()-1;i>=0;i--)
+    {
+        if(targetFileLoc.at(i)=='/')
+        {
+            targetFileName=targetFileLoc.substr((i+1),targetFileLoc.length()-(i+1));
+            targetFileLoc=targetFileLoc.substr(0,i+1);
+            break;
+        }
+    }
+}
+
+void getLocations()
+{
+    cout<<"Enter source file location:";
+    getline(cin,targetFileLoc);
+    getTargetFileName();
+    cout<<"Specify output file location (Default Desktop):";
+    getline(cin,destLoc);
+    if(destLoc.empty())
+        destLoc="~/Desktop/";
+    cout<<"Specify output file name (Default VidConvOutput):";
+    getline(cin,outFileName);
+    if(outFileName.empty())
+        outFileName="VidCutOutput";
+    dispLoc();
+}
+
+void dispLoc()
+{
+    cout<<"\nInput File Location:"<<targetFileLoc<<endl;
+    cout<<"Input File Name:"<<targetFileName<<endl;
+    cout<<"\nDestination:"<<destLoc<<endl;
+    cout<<"Output File Name:"<<outFileName<<endl;
+    sleep(7);
+}
 //Function Definations
 void resetMainMenuFlag()
 {
@@ -27,10 +68,9 @@ void greet()
 }
 void printMenu(string menu[],int length)
 {
-   // cout<<"\n\t\t\t\tMake your choice !!\n";
     for(int i=0;i<length;i++)
     {
-        cout<<"("<<(i+1)<<")"<<menu[i]<<endl;
+       cout<<"["<<(i+1)<<"] "<<menu[i]<<endl;
     }
 }
 void resetChoice()
@@ -86,6 +126,7 @@ int mainMenu()
                         sleep(2);
                      
         }
+        system("Color 02");
     }
     return 0;
 }
@@ -105,13 +146,13 @@ int convMenu()
             case 1: {
                     resetChoice();
                     updateCurrPage("main/convert/Video/");
-                    //dummy call to convert video fn
+                    convVid();
                     break;
                     }
             case 2: {
                     resetChoice();
                     updateCurrPage("main/convert/Audio/");
-                    //dummy call to convert video fn
+                    convAud();
                     break;
                     }
             case 3: return 0;
@@ -124,3 +165,136 @@ int convMenu()
     }
     return 0;
 }
+int convVid()
+{
+   resetChoice();
+   updateCurrPage("main/convert/Video/");
+   string menu[]={"Video->Audio","Video->Video (Other Format)","Back","Exit"};
+    while(1)
+    {   
+        cls();
+        cout<<"--------[Convert Video Menu]-------\n\n"<<endl;
+        printMenu(menu,sizeof(menu)/sizeof(menu[0]));
+        choice=inputChoice();
+        switch(choice)
+        {
+            case 1: {
+                    resetChoice();
+                    updateCurrPage("main/convert/Video_Audio/");
+                    audioFormat(0);
+                    break;
+                    }
+            case 2: {
+                    resetChoice();
+                    updateCurrPage("main/convert/Video_Video/");
+                    //dummy call to convert video->video (other format) fn
+                    break;
+                    }
+            case 3: return 0;
+            case 4: exit(0);
+            default:    resetChoice();
+                        cout<<"Please enter a valid input !!"<<endl;
+                        sleep(2);
+        }
+    }
+    return 0;
+}
+int convAud()
+{
+   resetChoice();
+   updateCurrPage("main/convert/Audio/");
+   string menu[]={"Add Album art to audio","Audio->Audio (Other Format)","Back","Exit"};
+    while(1)
+    {   
+        cls();
+        cout<<"--------[Convert Audio Menu]-------\n\n"<<endl;
+        printMenu(menu,sizeof(menu)/sizeof(menu[0]));
+        choice=inputChoice();
+        switch(choice)
+        {
+            case 1: {
+                    resetChoice();
+                    updateCurrPage("main/convert/Audio/PictToAudio/");
+                    //dummy call to add picture to audio fn
+                    break;
+                    }
+            case 2: {
+                    resetChoice();
+                    updateCurrPage("main/convert/Audio/Audio_Audio/");
+                    //dummy call to convert audio->audio (other format) fn
+                    break;
+                    }
+            case 3: return 0;
+            case 4: exit(0);
+            default:    resetChoice();
+                        cout<<"Please enter a valid input !!"<<endl;
+                        sleep(2);
+        }
+    }
+    return 0;
+}
+
+//Formats for Audio/Video
+
+int audioFormat(int flag)
+{
+    int status=0;
+   resetChoice();
+   //updateCurrPage("main/convert/Audio/");
+   string menu[]={"mp3","flac","wav","ogg","Back","Exit"};
+   string command="";
+    while(1)
+    {   
+        cls();
+        if(flag==0)
+        cout<<"--------[Convert Video->Audio Menu]-------\n\n"<<endl;
+        if(flag==1)
+        cout<<"--------[Convert Audio->Audio\t(Other Format) Menu]-------\n\n"<<endl;
+        printMenu(menu,sizeof(menu)/sizeof(menu[0]));
+        choice=inputChoice();
+        if(choice!=5 && choice!=6)
+        getLocations();
+        switch(choice)
+        {
+            case 1:
+                    command="ffmpeg -i "+targetFileLoc+"\""+targetFileName+"\""+" -vn "+destLoc+"\""+outFileName+"."+menu[0]+"\"";
+                    break;
+            case 2: 
+                    command="ffmpeg -i "+targetFileLoc+"\""+targetFileName+"\""+ " -vn -c:a flac "+destLoc+"\""+outFileName+"."+menu[1]+"\"";
+                    break;
+            case 3:
+                    command="ffmpeg -i "+targetFileLoc+"\""+targetFileName+"\""+ " -vn -acodec pcm_s16le "+destLoc+"\""+outFileName+"."+menu[2]+"\"";
+                    break;
+            case 4:
+                    command="ffmpeg -i "+targetFileLoc+"\""+targetFileName+"\""+ " -vn -acodec libvorbis "+destLoc+"\""+outFileName+"."+menu[3]+"\"";
+                    break;
+                   
+            case 5: return 0;
+
+            case 6: exit(1);
+
+            default:   
+                        resetChoice();
+                        cout<<"Please enter a valid input !!"<<endl;
+                        sleep(2);
+        }
+
+                    resetChoice();
+                    //cout<<"\nExecuting :$"<<command<<endl;
+                    //sleep(20);
+                    status=system(command.c_str());
+                    if(status==0)
+                    {
+                        cls();
+                        cout<<"\n\n\n\t\t\t\tFile convertion Successful Yay!! (:"<<endl;
+                        sleep(3);
+                    }
+                    else{
+                        cls();
+                        cout<<"\n\n\n\t\t\t\tFile Conversion Failed (T_T) "<<endl;
+                        sleep(3);
+                    }
+    }
+    return 0; 
+}
+
--- menu.hpp
+++ menu.hpp
@@ -1,6 +1,7 @@
 /*Menu.hpp*/
 #ifndef MENU
 #define MENU
+#include <stdlib.h>
 #include <string>
 #include <limits>
 #include <unistd.h>
@@ -9,16 +10,31 @@ using namespace std;
 extern int choice;
 extern string currPage;
 extern int mainMenuFlag;
+
+extern string targetFileLoc;
+extern string destLoc;
+extern string outFileName;
+extern string targetFileName;
+
+
+void getTargetFileName();
+void dispLoc();
 void greet();
 void printMenu(string [],int);
 void updateCurrPage(string);
 void resetChoice();
 void cls();
 void resetMainMenuFlag();
+void getLocations();
 
 int inputChoice();
 //menu declaration
 int mainMenu();
 int convMenu();
+int convVid();
+int convAud();
 
+
+//Formats for Audio/Video
+int audioFormat(int);
 #endif
